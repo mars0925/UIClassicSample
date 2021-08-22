be84e274d409262b553c897d230ff86cf7197e00
mars <mars09250148@gmail.com>
2021-08-22 22:40:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uiclassicsample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.uiclassicsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 單元 1 - 4_1 照片牆 Staggered Grid Layout manager
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
